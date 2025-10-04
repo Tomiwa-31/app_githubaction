e671035ff541a9bb8840b7ee5cda00171036b309
Tomiwa <oladimejitomiwa31@gmail.com>
2025-10-04 17:48:53 +0100

    modifying test values to the ccorrect ones

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -9,6 +9,6 @@ def test_add():
     
 def test_sub():
     assert sub(5,3) == 2
-    assert add(4,3) == 1
-    assert add(3,3) == 0
-    assert add(2,3) == -1 
+    assert sub(4,3) == 1
+    assert sub(3,3) == 0
+    assert sub(2,3) == -1 
